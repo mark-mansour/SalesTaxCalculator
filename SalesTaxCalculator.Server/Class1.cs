f623bac52431bebcfb764220329afa025a2e82cf
markm <markm@markorg>
2021-07-03 17:09:37 -0400

    Sales Tax Calculator Initial Solution and Projects

---
+++ SalesTaxCalculator.Server/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SalesTaxCalculator.Server
+{
+    public class Class1
+    {
+    }
+}
---
+++ SalesTaxLibrary.Shared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace SalesTaxCalculator.Shared
+{
+    public class Class1
+    {
+    }
+}
